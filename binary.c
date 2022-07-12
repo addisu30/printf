9834fef411fa42c25a1213cdcbd708606e80bf0e
addisu30 <105362027+addisu30@users.noreply.github.com>
2022-07-12 17:25:18 +0300

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,33 +1,34 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * conversion_b - checks validity of binaries
- * @s: format string ot check
- * Return: 1 if checks and 0 and exits otherwise
+ * printf_bin - prints a binary number.
+ * @val: arguments.
+ * Return: 1.
  */
-int conversion_b(char *s)
+int printf_bin(va_list val)
 {
-(void) s;
-return (1);
-}
+int flag = 0;
+int cont = 0;
+int i, a = 1, b;
+unsigned int num = va_arg(val, unsigned int);
+unsigned int p;
 
-
-/**
- * make_binary - make an octal string
- * @s: a format string
- * @l: a va_list
- * Return: a pointer to the result
- */
-char *make_binary(char *s, va_list l)
+for (i = 0; i < 32; i++)
 {
-unsigned int n;
-char *result;
-(void) s;
-
-n = va_arg(l, unsigned int);
-result = octorbi(n, 2);
-return (result);
+p = ((a << (31 - i)) & num);
+if (p >> (31 - i))
+flag = 1;
+if (flag)
+		{
+b = p >> (31 - i);
+_putchar(b + 48);
+cont++;
 }
-
+}
+if (cont == 0)
+{
+cont++;
+_putchar('0');
+}
+return (cont);
+}
